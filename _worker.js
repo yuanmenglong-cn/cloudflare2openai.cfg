192606a818c90516de7e645b9f6ddffbf670ae9b
yuanmenglong-cn <68841196+yuanmenglong-cn@users.noreply.github.com>
2024-05-23 16:14:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,3 +1,18 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    url.host = "api.openai.com";
+    // openai is already set all CORS heasders 
+    return fetch(url, {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: 'follow'
+    });
+  }
+}
+
+
 const TELEGRAPH_URL = 'https://api.openai.com';
 
 addEventListener('fetch', event => {
